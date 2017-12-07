f0bdd819c27eeaf219adabaccb210132428b1c66
MarieJohanna <mariejohannanilsson@gmail.com>
2017-12-07 17:20:43 +0100

    personal information

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -6,23 +6,49 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-start">
-        <div className="top-of-page">
-          <div className="image-box">
-            <img src="" alt=""/>
-          </div>
-        </div>
         <header>
           <nav className="navigation">
             <ul id="sub-nav-1">
               <li>About</li>
               <li>Projects</li>
               <li>Work</li>
-            </ul>
-            <ul id="sub-nav-2">
               <li>Contact</li>
             </ul>
           </nav>
         </header>
+        <div className="top-of-page">
+          <div className="image-box">
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="short-information">
+          <div className="short-about">
+            <div className="text-box">
+              Project 1 - text
+            </div>
+          </div>
+          <div className="project">
+            Project 2
+          </div>
+        </div>
+        <div className="personal-highlight">
+          <div className="hobbies-text-box">
+            <h3>Hobbies</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Totam in perspiciatis rerum, tenetur vitae animi eum non
+               aliquam cupiditate ea. Alias, repudiandae, quo eveniet
+               officiis voluptatem similique dolore consequatur quod?
+            </p>
+          </div>
+          <div className="hobbies-box">
+            <h3>Hobbies</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Totam in perspiciatis rerum, tenetur vitae animi eum non
+               aliquam cupiditate ea. Alias, repudiandae, quo eveniet
+               officiis voluptatem similique dolore consequatur quod?
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
