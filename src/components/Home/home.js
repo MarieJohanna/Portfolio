3e7d3446ce792329a7621f2c774c55ec79de257e
MarieJohanna <mariejohannanilsson@gmail.com>
2018-01-14 18:27:12 +0100

    responsive nav

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,7 @@
 import React from "react"
 import Grid from "./Grid/grid"
 import Name from "./Name/name"
-// import About from "../About/about"
-// import Grid from "../Home/Grid/grid"
 import Navigation from "../Navigation/navigation"
-// import Form from "../Form/form"
 import "./home.css"
 
 class Home extends React.Component {
@@ -14,46 +11,7 @@ class Home extends React.Component {
       <div className="home-start">
         <Navigation />
         <Name />
-
         <Grid />
-        {/* <Form /> */}
-        {/* <div className="top-of-page">
-          <div className="image-box" />
-          <div className="triangles">
-            <div className="triangle-1" />
-            <div className="triangle-2" />
-          </div>
-        <div className="content">
-          <div className="short-information">
-            <div className="short-about">
-              <div className="short-about-text">
-                <h3>About Marie</h3>
-                <p>This is me...</p>
-              </div>
-            </div>
-            <div className="project">
-              <div className="project-1">
-                <h3>Project 1</h3>
-              </div>
-              <div className="project-2">
-                <h3>Project 2</h3>
-              </div>
-            </div>
-          </div>
-          <div className="personal-highlight">
-            <div className="hobbies-text-box">
-              <h3>Ice Cream</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Totam in perspiciatis rerum, tenetur vitae animi eum non
-               aliquam cupiditate ea. Alias, repudiandae, quo eveniet
-               officiis voluptatem similique dolore consequatur quod?
-              </p>
-            </div>
-            <div className="hobbies-box">
-              <h3>Bild</h3>
-            </div>
-          </div>
-        </div> */}
       </div>
     )
   }
